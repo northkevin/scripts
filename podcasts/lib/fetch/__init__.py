001778f8184c5d5446d17a670ae624f06c82b6fe
northkevin <dev@northkevin.com>
2024-12-31 03:25:28 -0700

    remove podcasts cli from Scripts and move to new repo project

--- podcasts/lib/fetch/__init__.py
+++
@@ -1,9 +0,0 @@
-from .vimeo import process_vimeo_transcript, get_vimeo_data_headless, create_episode_metadata
-from .youtube import YouTubeFetcher
-
-__all__ = [
-    'process_vimeo_transcript',
-    'get_vimeo_data_headless',
-    'create_episode_metadata',
-    'YouTubeFetcher'
-]
--- podcasts/lib/generators/__init__.py
+++
@@ -1,8 +0,0 @@
-from .id import PodcastID, IDGenerator
-from .markdown import MarkdownGenerator
-
-__all__ = [
-    'PodcastID',
-    'IDGenerator',
-    'MarkdownGenerator'
-]
